74e792c2d2d4cfef60e4fa6b0bc4d50a55b2a67c
IRyLeXI <maksgavalec@gmail.com>
2023-04-25 01:52:24 +0300

    Meteor and script for it

---
+++ Spacing Out/Assets/Scripts/MeteorScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorScript : MonoBehaviour
+{
+
+    public float RotationSpeed;
+    
+    public Vector2 Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateMeteor();
+        MoveMeteor();
+    }
+
+    private void RotateMeteor()
+    {
+        float newZ = transform.rotation.eulerAngles.z + (RotationSpeed * Time.deltaTime);
+        Vector3 newR = new (0, 0, newZ);
+        transform.rotation = Quaternion.Euler(newR);
+    }
+
+    private void MoveMeteor()
+    {
+        float newX = transform.position.x + (Speed.x * Time.deltaTime);
+        float newY = transform.position.y + (Speed.y * Time.deltaTime);
+        transform.position = new Vector2(newX, newY); 
+    }
+
+}

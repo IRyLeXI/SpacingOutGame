44a9d7746708bc67b899ee3c570e28ccd6110fef
IRyLeXI <maksgavalec@gmail.com>
2023-04-25 01:14:56 +0300

    Added script for Shuttle movement

--- Spacing Out/Assets/Scripts/PlayerController.cs
+++ Spacing Out/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public int Speed = 1;
+
+    public Vector2 Velocity = new Vector2(0, 0);
+
+    public Vector2 Min, Max;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Velocity = HandleHorizontal(Input.GetAxis("Horizontal"));
+        Velocity += HandleVertical(Input.GetAxis("Vertical"));
+        MoveShip();
+    }
+
+    private Vector2 HandleHorizontal(float h)
+    {
+        return new Vector2(Mathf.Clamp(h,-1,1), 0);
+    }
+
+    private Vector2 HandleVertical(float v)
+    {
+        return new Vector2(0, Mathf.Clamp(v,-1,1));
+    }
+
+    private void MoveShip()
+    {
+        float newX = transform.position.x + (Velocity.x * Speed * Time.deltaTime);
+        float newY = transform.position.y + (Velocity.y * Speed * Time.deltaTime);
+        newX = Mathf.Clamp(newX, Min.x, Max.x);
+        newY = Mathf.Clamp(newY, Min.y, Max.y);
+        transform.position = new Vector2(newX, newY); 
     }
 }
